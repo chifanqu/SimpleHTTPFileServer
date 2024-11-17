f311957169fac542be4934e51b7d7895d73e8a35
chifanqu <58767229+chifanqu@users.noreply.github.com>
2024-11-17 17:22:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
 # !/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from setuptools import setup, find_packages    
+from setuptools import setup, find_packages
     
-setup(    
+setup(
     name = "SimpleHTTPFileServer",
-    version = "0.1",    
-    py_modules=['SimpleHTTPFileServer'],  
+    version = "0.2",
+    py_modules=['SimpleHTTPFileServer'],
     
-    description = "Simple HTTP Server With POST and PUT Upload.",    
-    long_description = "Simple HTTP Server With POST and PUT Upload.",    
-    author = "chifanqu",    
-    author_email = "yiyiyaya1@139.com",    
+    packages = ['SimpleHTTPFileServer2','SimpleHTTPFileServer3'],
+    
+    description = "Simple HTTP Server With POST and PUT Upload.",
+    long_description = "Simple HTTP Server With POST and PUT Upload.",
+    author = "chifanqu",
+    author_email = "yiyiyaya1@139.com",
 
-    python_requires='>=2.7, <3',
-       
-    keywords = ("file server", "put"),    
-    platforms = "Independant",    
-    url = "https://github.com/chifanqu/SimpleHTTPFileServer",  
-    entry_points = {  
-        'console_scripts': [  
+    python_requires='>=2.7',
+
+    keywords = ("file server", "put"),
+    platforms = "Independant",
+    url = "https://github.com/chifanqu/SimpleHTTPFileServer",
+    entry_points = {
+        'console_scripts': [
             'SimpleHTTPFileServer = SimpleHTTPFileServer:main'
-        ]  
-    }  
+        ]
+    }
 )
